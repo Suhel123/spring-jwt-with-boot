e8f5ee9da7d1cdaa1171b785cdc9e0b82cf746ce
Suhelahmad#12 <suhailakhan86@gmail.com>
2023-09-23 11:53:53 +0530

    new chnege in model

--- src/main/java/spring/jwt/user/User1.java
+++ src/main/java/spring/jwt/user/User1.java
@@ -2,6 +2,27 @@ package spring.jwt.user;
 
 public class User1 {
 
+ int id;
+ 
+ String username;
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+ 
+ 
 	
 
 }
